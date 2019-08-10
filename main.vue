5a4024a03423de32c01c60cbca92743bf3aea673
lastboy1228 <lastboy1228@gmail.com>
2019-08-10 20:53:39 +0800

    move bg image

--- main.vue
+++ main.vue
@@ -515,7 +515,7 @@ module.exports = {
   padding: 0;
   width: 100%;
   height: 100%;
-  background-image: url("honeycomb.png");
+  background-image: url("./images/honeycomb.png");
   background-size: cover;
   overflow: hidden;
 }
